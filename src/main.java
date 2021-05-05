438b72d7beb66d10d2d63bd352dc7d5692fce72e
Pablo Ramos M <pablotec123@estudiantec.cr>
2021-05-05 16:45:50 -0600

    First commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        int arr[] = { 12, 11, 13, 5, 6 };
+
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+        InsertionSort.printArray(arr);
+    }
+
+
+
+
+
+
+}
